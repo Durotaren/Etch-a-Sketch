2c411139bf4728ee6730388c807577073f8d182a
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-04-25 15:36:34 +0300

    Set up the button interactivity now aswell

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const container = document.querySelector('#container');
-console.log(container);
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 4096; i++) {
   let div = document.createElement('div');
   div.classList.add('box');
-  // div.textContent = 'Hey';
   container.append(div);
 }
 
@@ -14,11 +12,33 @@ boxes.forEach((box) => {
   box.addEventListener('mouseenter', () => 
     box.style.backgroundColor = 'blue'
   );
-})
+});
 
-boxes.forEach((box) => {
-  box.addEventListener('mouseleave', () => 
-    box.style.backgroundColor = 'aliceblue'
-  );
-})
+let button = document.querySelector('button');
 
+function boxCount() {
+  userAnswer = prompt('How many boxes would you like to be on the grid?');
+  let boxSize = 640 / userAnswer;
+  boxes.forEach((box) => {
+    box.remove();
+  });
+  for (let i = 0; i < (userAnswer * userAnswer); i++) {
+    let div = document.createElement('div');
+    div.classList.add('box');
+    container.append(div);
+  }
+
+  let newBoxes = document.querySelectorAll('.box');
+
+  newBoxes.forEach((box) => {
+    box.style.cssText = `width:${boxSize}px; height:${boxSize}px`;
+  });
+
+  newBoxes.forEach((box) => {
+    box.addEventListener('mouseenter', () => 
+      box.style.backgroundColor = 'blue'
+    );
+  });
+}
+
+button.addEventListener('click', boxCount);
